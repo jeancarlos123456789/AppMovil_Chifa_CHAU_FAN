e3dc25b1c78e3b15636bfe16a9666e2c07b05e6c
unknown <Admin@IHR80PBF271.continental.edu.pe>
2019-12-14 05:48:58 -0500

    Splash Screen -- Login

--- app/src/main/java/com/example/chifachaufanapplication/LoginActivity.java
+++ app/src/main/java/com/example/chifachaufanapplication/LoginActivity.java
@@ -2,13 +2,56 @@ package com.example.chifachaufanapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.chifachaufanapplication.Models.user;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText txtUser;
+    EditText txtPassword;
+
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        txtUser = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPassword);
+
+        btnRegistrar = findViewById(R.id.btnregistrar);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.passwordIsValid(txtUser.getText().toString(),
+                        txtPassword.getText().toString())){
+                    Intent o = new Intent(LoginActivity.this,
+                            MainActivity.class);
+                    startActivity(o);
+                }else{
+
+                }
+
+
+            }
+        });
+         btnRegistrar.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent o = new Intent(LoginActivity.this,
+                         MainActivity.class);
+                 startActivity(o);
+             }
+         });
+
+
     }
 }
--- app/src/main/java/com/example/chifachaufanapplication/SplashScreenActivity.java
+++ app/src/main/java/com/example/chifachaufanapplication/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.example.chifachaufanapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -9,6 +10,11 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+
+        Intent o = new Intent(this, LoginActivity.class);
+        startActivity(o);
+        finish();
+
+        //setContentView(R.layout.activity_splash_screen);
     }
 }
